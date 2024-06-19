24a6ad8f4371cded4fa540ac354f0653ac227f8a
Ziad Hamdi Saad <130312784+ziad768@users.noreply.github.com>
2024-06-20 01:16:15 +0300

    Update error.js

--- app/error.js
+++ app/error.js
@@ -1,7 +1,3 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
 export default function Error({
   error,
   reset,
@@ -9,22 +5,10 @@ export default function Error({
   error: Error & { digest?: string }
   reset: () => void
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+    </>
   )
 }
